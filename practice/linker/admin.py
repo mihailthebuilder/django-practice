f13291d11c6e4b487f312f1e63aedfcb9d52399e
Mihail Marian <mihailmarian12@gmail.com>
2021-02-07 12:04:51 +0000

    added models

--- practice/linker/admin.py
+++ practice/linker/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Message
+from .models import Message, Comment
 
 # Register your models here.
 admin.site.register(Message)
+admin.site.register(Comment)
--- practice/linker/models.py
+++ practice/linker/models.py
@@ -2,16 +2,33 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class Message(models.Model):
-    message_title = models.CharField(max_length=100)
-    message_text = models.TextField()
-    pub_date = models.DateTimeField("date published", auto_now_add=True)
+class CommonTextModel(models.Model):
+    text_content = models.TextField()
+    pub_date = models.DateTimeField("date published", blank=True)
+    votes = models.PositiveSmallIntegerField(default=0)
 
     def save(self, *args, **kwargs):
         """on save, update timestamp"""
-        if not self.id:
-            self.created = timezone.now()
-        return super(Message, self).save(*args, **kwargs)
+        self.pub_date = timezone.now()
+        return super(CommonTextModel, self).save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+
+
+class Message(CommonTextModel):
+    message_title = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.pub_date.strftime("%d/%m/%Y") + "|| " + self.message_title
+        return self.pub_date.strftime("%d/%m/%Y") + " || " + self.message_title
+
+
+class Comment(CommonTextModel):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+
+    def __str__(self):
+        str_self_text = self.text_content
+        if len(str_self_text) > 40:
+            str_self_text = str_self_text[:40] + "..."
+
+        return self.pub_date.strftime("%d/%m/%Y") + " || " + str_self_text
