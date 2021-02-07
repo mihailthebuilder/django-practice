c77ffee0117801bb2d1f3eca9136c3dafd4b3fd4
Mihail Marian <mihailmarian12@gmail.com>
2021-02-07 11:28:01 +0000

    created messages model

--- practice/linker/models.py
+++ practice/linker/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Message(models.Model):
     message_title = models.CharField(max_length=100)
     message_text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        """on save, update timestamp"""
+        if not self.id:
+            self.created = timezone.now()
+        return super(Message, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.pub_date.strftime("%d/%m/%Y") + "|| " + self.message_title
