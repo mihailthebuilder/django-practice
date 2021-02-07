0c31df9e6b0eacb6d434b64275e2102a6bae689b
Mihail Marian <mihailmarian12@gmail.com>
2021-02-07 09:32:47 +0000

    set up different pages

--- practice/linker/views.py
+++ practice/linker/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome to the homepage.")
+
+
+def contact(request):
+    return HttpResponse("Contact me here.")
+
+
+def about(request):
+    return HttpResponse("Find out more about me.")
