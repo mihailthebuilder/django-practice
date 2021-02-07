d60c6b9185b45cd53afec227e8edc71cadba6cad
Mihail Marian <mihailmarian12@gmail.com>
2021-02-07 18:32:03 +0000

    added links to other pages

--- practice/linker/urls.py
+++ practice/linker/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "linker"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("contact/", views.contact, name="contact"),
