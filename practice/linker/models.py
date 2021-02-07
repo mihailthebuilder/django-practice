f3e6fa464a0ec07a520224f86e697a696bb2669a
Mihail Marian <mihailmarian12@gmail.com>
2021-02-07 11:07:52 +0000

    created message model

--- practice/linker/models.py
+++ practice/linker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    message_title = models.CharField(max_length=100)
+    message_text = models.TextField()
+    pub_date = models.DateTimeField("date published", auto_now_add=True)
+
+    def __str__(self):
+        return self.pub_date.strftime("%d/%m/%Y") + "|| " + self.message_title
