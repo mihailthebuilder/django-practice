507b4717677df9c5f4ee3f7132b143648941562f
Mihail Marian <mihailmarian12@gmail.com>
2021-02-08 14:39:16 +0000

    simplified saving

--- practice/linker/models.py
+++ practice/linker/models.py
@@ -4,16 +4,11 @@ from django.utils import timezone
 # Create your models here.
 class CommonTextModel(models.Model):
     text_content = models.TextField()
-    pub_date = models.DateTimeField("date published", blank=True)
+    pub_date = models.DateTimeField("date published", auto_now_add=True)
     votes = models.SmallIntegerField(default=0)
-    formatted_date = models.CharField(max_length=40, blank=True)
 
-    def save(self, *args, **kwargs):
-        """on save, update timestamp"""
-        if not self.id:
-            self.pub_date = timezone.now()
-        self.formatted_date = self.pub_date.strftime("%d/%m/%Y | %H:%M")
-        return super(CommonTextModel, self).save(*args, **kwargs)
+    def formatted_date(self):
+        return self.pub_date.strftime("%d/%m/%Y | %H:%M")
 
     class Meta:
         abstract = True
@@ -23,7 +18,7 @@ class Message(CommonTextModel):
     message_title = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.formatted_date + " || " + self.message_title
+        return self.formatted_date() + " || " + self.message_title
 
 
 class Comment(CommonTextModel):
@@ -34,4 +29,4 @@ class Comment(CommonTextModel):
         if len(str_self_text) > 40:
             str_self_text = str_self_text[:40] + "..."
 
-        return self.formatted_date + " || " + str_self_text
+        return self.formatted_date() + " || " + str_self_text
